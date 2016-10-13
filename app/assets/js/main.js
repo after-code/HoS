2ed0ac2cefd42e311ab8dd86076e986aedabdf93
_vasilie <vasilije.milenkovic@diwanee.com>
2016-10-13 16:11:46 +0200

    header motion

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -3,6 +3,7 @@
   var accelerationCurve = [0.4, 0.0, 1, 1];
   var sharpCurve = [0.4, 0.0, 0.6, 1];
   var standardCurve = [0.4, 0.0, 0.2, 1];
+  var longCurve = [0.7, 0.0, 0.2, 1];
   var open = false;
   var animating = false;
   var animSpeed = 205;
@@ -45,26 +46,26 @@ $(function(){
 
       console.log(header);
           if ( x1 == "REVERSE" && x2 >= x3 && i1 == 0 ) {
-              TweenLite.fromTo(".header", 0.24,{height:"97px" }, {position:"fixed", color:"white",  top: "0px", delay:.2,  ease:  $.bez(accelerationCurve), onComplete:headerBlack});
-              TweenLite.fromTo(".header__logo--white", 0, {width:"142px", "margin-top": '1.1%', left: '25.7%' }, {opacity:'1', ease:  $.bez(accelerationCurve)});
-              TweenLite.to(".header__logo", 0, {opacity:'0', ease:  $.bez(accelerationCurve)});
-              TweenLite.to(".header__links", 0, {"margin-top":"0%", ease:  $.bez(accelerationCurve)});
+              TweenLite.to(".header", 0.24, {position:"fixed", color:"white",  top: "0px", delay:.2,  ease:  $.bez(decelerationCurve), onComplete:headerBlack});
+              TweenLite.fromTo(".header__logo--white", 0, {width:"10%", "margin-top": '1.1%', left: '25.7%' }, {opacity:'1', ease:  $.bez(decelerationCurve)});
+              TweenLite.to(".header__logo", 0, {opacity:'0', ease:  $.bez(decelerationCurve)});
+              TweenLite.to(".header__links", 0, {"margin-top":"2.8%", "margin-bottom":"2.8%",ease:  $.bez(decelerationCurve)});
               i1++;
               i2 = 0;
           }
           if ( x1 == "REVERSE" && x2 == 0 && header == "black") {
-              TweenLite.fromTo(".header", 0.28, { position:"relative", top: "0px"}, {height:"149px",top: "0px", ease:  $.bez(decelerationCurve), onComplete:function(){
-                TweenLite.to(".header", 0.55, {color:"rgb(34, 34, 34)", background:"rgb(255,255,255)"});
-                TweenLite.to(".header__logo", 0.55, {opacity:'1'});
-                TweenLite.to(".header__logo--white", 0.55, {opacity:'0'});
+              TweenLite.to(".header", 0.28, {top: "0px", ease:  $.bez(decelerationCurve), onComplete:function(){
+                TweenLite.to(".header", 0.42, {color:"rgb(34, 34, 34)", background:"rgb(255,255,255)",  ease:  $.bez(standardCurve)});
+                TweenLite.to(".header__logo", 0.42, {opacity:'1',delay:0.12, ease:  $.bez(standardCurve)});
+                TweenLite.to(".header__logo--white", 0.42, {opacity:'0',delay:0.12, ease:  $.bez(standardCurve), onComplete:headerWhite});
               }});
 
-              TweenLite.to(".header__links", 0.28, {"margin-top":"2%", ease:  $.bez(decelerationCurve)});
-              TweenLite.to(".header__logo--white", 0.28, {width:"185px", left:"22.7%", "margin-top":"2.4%", ease:  $.bez(decelerationCurve)});
+              TweenLite.to(".header__links", 0.28, {"margin-top":"4.16%","margin-bottom":"4.16%", delay:0.18, ease:  $.bez(decelerationCurve)});
+              TweenLite.to(".header__logo--white", 0.28, {width:"11%", left:"24.7%", ease:  $.bez(sharpCurve)});
+              TweenLite.to(".header__logo--white", 0.28, {"margin-top":"2.4%", delay:0.18, ease:  $.bez(decelerationCurve)});
               i1 = 0;
               i2 = 0;
             }
-
           if ( x1 == "FORWARD" && x2 <= x3 ) {
               TweenLite.to( ".header", 0.4, {position:"relative", background: "white", top:"0px", ease: $.bez(accelerationCurve) });
               i1 = 0;
@@ -86,6 +87,8 @@ $(function(){
   $(".footer__back-to-top").click(function(){
     $("html, body").animate({scrollTop:"0px"},1000, $.bez(accelerationCurve));
   });
+  var header_height = $(".header__nav").height();
+  $(".header__wrap").css({"height":header_height+"px"});
 });
 function headerBlack(){
   header = 'black';
