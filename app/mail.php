5dcda2c666e26c021049cbd03dd2d90049486f17
Vasilije Milenkovic <vasilie@Vasilijes-MacBook-Pro.local>
2016-10-31 21:52:31 +0100

    mailer, ebooks

--- app/mail.php
+++ app/mail.php
@@ -1,35 +1,41 @@
 <?php
-
-
-
 require_once 'vendor/phpmailer/PHPMailerAutoload.php';
 
-header('Content-Type: application/json');
-$fname = $_POST['name'];
-$lname = $_POST['surname'];
-$mail = $_POST['email'];
-$subject = $_POST['sub'];
-$message = $_POST['mssg'];
-$tel = $_POST['tel'];
+// echo 'hey';
 
+
+// header('Content-Type: application/json');
+// $fname = $_POST['name'];
+// $lname = $_POST['surname'];
+$mail = $_POST['email'];
+// $mail = "vasilie.91@gmail.com";
+
+
+// $subject = $_POST['sub'];
+$message = $_POST['themessage'];
+// $message = 'vasa';
+
+
+// $tel = $_POST['tel'];
+echo $mail;
 $mail = new PHPMailer;
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'wc-122.atnoc.com';  // Specify main and backup SMTP servers
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'support@xxxxxx.com';                 // SMTP username
-$mail->Password = 'xxxxxxxxxxxxx';                           // SMTP password
+$mail->Username = 'vasilie.91@gmail.com';                 // SMTP username
+$mail->Password = 'winstonbluetacka1';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 25;                                    // TCP port to connect to
 
-$mail->setFrom($_POST['email'], $_POST['fname']);
+$mail->setFrom($_POST['email']);
 // Add a recipient
-$mail->addAddress('rseifi.a73@gmail.com');               // Name is optional
+$mail->addAddress('vasilie.91@gmail.com');               // Name is optional
 
 
 $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = $subject;
-$mail->Body    = "$message Phone $tel";
+// $mail->Subject = $subject;
+$mail->Body    = $message;
 $mail->AltBody = $message;
 if(!$mail->send()) {
   echo 'Message could not be sent.';
---
+++ app/vendor/js/mail.js
@@ -0,0 +1,33 @@
+$(function () {
+    // $('#contact-form').validator();
+    console.log("paprike");
+    $('#mailMe').on('submit', function (e) {
+    // if (!e.isDefaultPrevented()) {
+        var url = "mail.php";
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: $('#mailMe').serialize(),
+            success: function (data){
+              console.log(data);
+            },
+            error: function(xhr, textStatus, error){
+              console.log(xhr.statusText);
+               console.log(textStatus);
+               console.log(error);
+           }
+            // {
+            //     var messageAlert = 'alert-' + data.type;
+            //     var messageText = data.message;
+            //
+            //     var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+            //     if (messageAlert && messageText) {
+            //         $('#mailMe').find('.messages').html(alertBox);
+            //         $('#mailMe')[0].reset();
+            //     }
+            // }
+        });
+        return false;
+    // }
+  })
+});
