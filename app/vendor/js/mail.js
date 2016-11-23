d4a525f0b82e46827361197f64bc6053fb52347d
_vasilie <vasilije.milenkovic@diwanee.com>
2016-11-23 16:34:44 +0100

    javascript enclosed in jquery

--- app/vendor/js/mail.js
+++ app/vendor/js/mail.js
@@ -1,34 +1,36 @@
-$(function () {
-    console.log("paprike");
-    $('#mailMe').on('submit', function (e) {
-    if (!e.isDefaultPrevented() ) {
-        var url = "mail.php";
-        $.ajax({
-            type: "POST",
-            url: url,
-            data: $('#mailMe').serialize(),
-            success: function (data){
-              console.log(data);
-              $('#mailMe')[0].reset();
-              hidePopup();
-            },
-            error: function(xhr, textStatus, error){
-              console.log(xhr.statusText);
-               console.log(textStatus);
-               console.log(error);
-           }
-            // {
-            //     var messageAlert = 'alert-' + data.type;
-            //     var messageText = data.message;
-            //
-            //     var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-            //     if (messageAlert && messageText) {
-            //         $('#mailMe').find('.messages').html(alertBox);
-            //         $('#mailMe')[0].reset();
-            //     }
-            // }
-        });
-        return false;
-    }
-  })
-});
+(function ($){
+  $(function () {
+      console.log("paprike");
+      $('#mailMe').on('submit', function (e) {
+      if (!e.isDefaultPrevented() ) {
+          var url = "mail.php";
+          $.ajax({
+              type: "POST",
+              url: url,
+              data: $('#mailMe').serialize(),
+              success: function (data){
+                console.log(data);
+                $('#mailMe')[0].reset();
+                hidePopup();
+              },
+              error: function(xhr, textStatus, error){
+                console.log(xhr.statusText);
+                 console.log(textStatus);
+                 console.log(error);
+             }
+              // {
+              //     var messageAlert = 'alert-' + data.type;
+              //     var messageText = data.message;
+              //
+              //     var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+              //     if (messageAlert && messageText) {
+              //         $('#mailMe').find('.messages').html(alertBox);
+              //         $('#mailMe')[0].reset();
+              //     }
+              // }
+          });
+          return false;
+      }
+    })
+  });
+})(jQuery);
